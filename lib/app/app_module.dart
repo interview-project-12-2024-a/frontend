e7707b7a3ebcd7259b586d24bb0baf1c346c2d10
Moises Quispe <moises.quispe.arellano@gmail.com>
2025-01-06 08:14:08 -0400

    refactor: remove chat store from app module

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -2,17 +2,13 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:frontend/app/modules/auth/auth_module.dart';
 import 'package:frontend/app/modules/auth/stores/auth_store.dart';
 import 'package:frontend/app/modules/chat/chat_module.dart';
-import 'package:frontend/app/modules/chat/controllers/chat_controller.dart';
-import 'package:frontend/app/modules/chat/stores/chat_store.dart';
 import 'package:frontend/app/modules/shared/guards/router_guard.dart';
-import 'package:frontend/app/modules/shared/services/web_service.dart';
 import 'app_routes.dart';
 
 class AppModule extends Module {
   @override
   void binds(i) {
     i.addSingleton(AuthStore.new);
-    i.addSingleton(ChatStore.new);
   }
 
   @override
--- lib/app/modules/chat/chat_module.dart
+++ lib/app/modules/chat/chat_module.dart
@@ -2,9 +2,13 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:frontend/app/modules/chat/chat_routes.dart';
 import 'package:frontend/app/modules/chat/views/chat_view.dart';
 
+import 'stores/chat_store.dart';
+
 class ChatModule extends Module {
   @override
-  void binds(i) {}
+  void binds(i) {
+    i.addSingleton(ChatStore.new);
+  }
 
   @override
   void routes(r) {
